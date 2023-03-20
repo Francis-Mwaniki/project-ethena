560d9e4d8b904f2feb91c61714ffb7a2c3a2a3df
Francis-Mwaniki <francismwaniki630>
2023-03-20 22:44:39 +0300

    ethena

---
+++ Backend/model/model.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const postSchema=new Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true},
+    message:{type:String,required:true}
+});
+module.exports=mongoose.model('Post',postSchema);
